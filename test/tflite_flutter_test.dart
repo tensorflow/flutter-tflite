cef1f4b8830133860fcfb0e2d86202d735f1e62d
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-08-03 22:12:34 +0200

    import correct package for dart analyze

--- test/tflite_flutter_test.dart
+++ test/tflite_flutter_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tflite_flutter/tflite_flutter_platform_interface.dart';
 import 'package:tflite_flutter/tflite_flutter_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
