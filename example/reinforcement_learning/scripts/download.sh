da093f2ff17772d91cbf751252ebb2f7f6d3a310
Wayne Wei <windmaple@gmail.com>
2023-09-02 09:33:15 +0800

    Update RL example model download path

--- example/reinforcement_learning/scripts/download.sh
+++ example/reinforcement_learning/scripts/download.sh
@@ -1,4 +1,4 @@
 # Download model
-curl https://github.com/tensorflow/examples/raw/master/lite/examples/reinforcement_learning/android/app/src/main/assets/planestrike_tf.tflite \
+curl https://storage.googleapis.com/download.tensorflow.org/models/tflite/flutter-plugin/reinforcement_learning/planestrike_tf.tflite \
     -o assets/models/planestrike.tflite \
     -L
