34452de11db80891987620aef642fc5b7bf2e90a
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-09-05 21:14:11 +0200

    updated old name

--- example/image_classification_mobilenet/test/widget_test.dart
+++ example/image_classification_mobilenet/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:image_classification_mobilenet/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const BottomNavigationBarApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
