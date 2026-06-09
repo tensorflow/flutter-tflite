42c844f5f057ff30793cbf62ca191a9a11912010
kmmhanan <kmm.hanan2@gmail.com>
2026-06-09 07:48:51 +0530

    fix: ios TensorFlowLiteC framework linked

--- ios/tflite_flutter/Package.swift
+++ ios/tflite_flutter/Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,21 +7,20 @@ let package = Package(
         .iOS("11.0"),
     ],
     products: [
-        .library(
-            name: "tflite_flutter",
-            targets: ["tflite_flutter"]
-        ),
-        .library(
-            name: "tflite-flutter",
-            targets: ["tflite_flutter"]
-        ),
+        .library(name: "tflite_flutter",  targets: ["tflite_flutter"]),
+        .library(name: "tflite-flutter",  targets: ["tflite_flutter"]),
     ],
-    dependencies: [],
     targets: [
+        .binaryTarget(
+            name: "TensorFlowLiteC",
+            url: "https://github.com/kewlbear/TensorFlowLiteC/releases/download/0.0.20240626/TensorFlowLiteC.xcframework.zip",
+            checksum: "3b527a7be16aa02f0900deb40d1561df839fdce96cf2d1aaa80388b5c28e2ac3"
+        ),
         .target(
             name: "tflite_flutter",
-            path: "Sources", 
-            resources: []
+            dependencies: ["TensorFlowLiteC"],
+            path: "Sources",
+            publicHeadersPath: "."
         ),
     ]
 )
